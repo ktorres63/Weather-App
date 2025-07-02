814a833b687817497c0864447e23dba153989bd8
ktorres63 <ktorresaro@unsa.edu.pe>
2025-07-01 23:30:51 -0500

    Update API endpoint in api.js to use HTTPS for secure data fetching.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export async function getWeather(city) {
   const API_KEY = "bf043589ba074fc6a0635815252506";
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
   if(!response.ok){
     throw new error (`Error: ${response.statusText}`);
   }
